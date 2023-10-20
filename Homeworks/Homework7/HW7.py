61328867b6f2b76c2abd7e64f4cad3f4d0284436
Maedee Trank-Greene <matr4909@colorado.edu>
2023-10-20 17:39:35 -0600

    Updated HW7 Code

--- Homeworks/Homework7/HW7.py
+++ Homeworks/Homework7/HW7.py
@@ -109,7 +109,7 @@ def driver4():
     plt.title("N = 17 Plot")
     plt.show()
         
-# Problem 2, i have a bug that I cannot figure out
+# Problem 2
 
 def driver5():
     N = 5
@@ -118,40 +118,294 @@ def driver5():
     for i in range(1, N + 1):
         xvals[i - 1] = -1 + (i - 1)*(2 / (N - 1))
 
-    print(xvals)
     f = lambda x: 1 / (1 + (10*x)**2)
     fvals = f(xvals)
     coefs = np.ones(N)
     yvals = np.zeros(N)
-    w = np.ones(N)
-    
-    for i in range(N):
-        for j in range(N):
-            if i != j:
-                w[i] *= (xvals[i] - xvals[j])**(-1)
     
     Neval = 1000
     xevals = np.linspace(-1, 1, Neval)
     yevals = np.zeros(Neval)
     
-    # for i in range(N):
-    #     for j in range(Neval):
-    #         print('x', xevals[j], 'xj', xvals[i])
-    #         coefs[i] *= xevals[j] - xvals[i]
-    
-    for j in range(Neval):
-        for i in range(N):
-            # print('x', xevals[j], 'i', i)
-            #print('x', xevals[j], 'xj', xvals[i])
-            coef = eval_coefs(i, xevals[j], xvals)
-            w = eval_w(i, j, xevals, xvals)
-            print(w)
-            # print('c', coef)
-            yevals[j] = coef*(w / (xevals[j] - xvals[i]))*f(xvals[i])
-
-    #print(yevals)
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+        
     plt.plot(xevals, f(xevals), 'ro-')
     plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 5 Barycentric Plot")
+    plt.show()
+    
+def driver6():
+    N = 8
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        xvals[i - 1] = -1 + (i - 1)*(2 / (N - 1))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+        
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 8 Barycentric Plot")
+    plt.show()
+    
+def driver7():
+    N = 10
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        xvals[i - 1] = -1 + (i - 1)*(2 / (N - 1))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+        
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 10 Barycentric Plot")
+    plt.show()
+
+def driver8():
+    N = 15
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        xvals[i - 1] = -1 + (i - 1)*(2 / (N - 1))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+        
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 15 Barycentric Plot")
+    plt.show()
+
+def driver9():
+    N = 20
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        xvals[i - 1] = -1 + (i - 1)*(2 / (N - 1))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 20 Barycentric Plot")
+    plt.show()
+    
+# Problem 3
+
+def driver10():
+    N = 5
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        # np.cos(((2*i - 1)*np.pi) / 2*N)
+        xvals[i - 1] = np.cos(((2*i - 1)*np.pi) / (2*N))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 5 Barycentric Plot")
+    plt.show()
+    
+def driver11():
+    N = 10
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        # np.cos(((2*i - 1)*np.pi) / 2*N)
+        xvals[i - 1] = np.cos(((2*i - 1)*np.pi) / (2*N))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 10 Barycentric Plot")
+    plt.show()
+    
+def driver12():
+    N = 15
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        # np.cos(((2*i - 1)*np.pi) / 2*N)
+        xvals[i - 1] = np.cos(((2*i - 1)*np.pi) / (2*N))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 15 Barycentric Plot")
+    plt.show()
+    
+def driver13():
+    N = 20
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        # np.cos(((2*i - 1)*np.pi) / 2*N)
+        xvals[i - 1] = np.cos(((2*i - 1)*np.pi) / (2*N))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 20 Barycentric Plot")
+    plt.show()
+    
+def driver14():
+    N = 30
+    xvals = np.zeros(N)
+
+    for i in range(1, N + 1):
+        # np.cos(((2*i - 1)*np.pi) / 2*N)
+        xvals[i - 1] = np.cos(((2*i - 1)*np.pi) / (2*N))
+
+    f = lambda x: 1 / (1 + (10*x)**2)
+    fvals = f(xvals)
+    coefs = np.ones(N)
+    yvals = np.zeros(N)
+    
+    Neval = 1000
+    xevals = np.linspace(-1, 1, Neval)
+    yevals = np.zeros(Neval)
+    
+    for i in range(Neval):
+        for j in range(N):
+            w = eval_w(N, j, xvals)
+            yevals[i] += (w / (xevals[i] - xvals[j]))*f(xvals[j])
+            
+        coef = eval_coefs(N, xevals[i], xvals)
+        yevals[i] = coef*yevals[i]
+            
+    plt.plot(xevals, f(xevals), 'ro-')
+    plt.plot(xevals, yevals, 'bo-')
+    plt.title("N = 30 Barycentric Plot")
     plt.show()
 
 def eval_coefs(N, x, xvals):
@@ -162,28 +416,16 @@ def eval_coefs(N, x, xvals):
         
     return coefs
 
-def eval_w(N, j, xevals,xvals):
+def eval_w(N, j, xvals):
     w = 1
-    x_j = xevals[j]
+    x_j = xvals[j]
     
     for i in range(N):
         if i != j:
             w *= (x_j - xvals[i])**(-1)
-        
-    
-    # for i in range(N):
-    #     for j in range(N):
-    #         if i != j:
-    #             w *= (xvals[i] - xvals[j])**(-1)
             
     return w
-    
-            
-#     plt.plot(xevals, f(xevals), 'ro-')
-#     plt.plot(xevals, yevals, 'bo-')
-#     plt.title("N = 17 Plot")
-#     plt.show()
-    
+
 def monomial(N, xvals, fvals):
     vander = np.zeros([N, N])
     
@@ -195,8 +437,17 @@ def monomial(N, xvals, fvals):
     
     return inverse.dot(fvals)
 
-# driver1()
-# driver2()
-# driver3()
-# driver4()
+driver1()
+driver2()
+driver3()
+driver4()
 driver5()
+driver6()
+driver7()
+driver8()
+driver9()
+driver10()
+driver11()
+driver12()
+driver13()
+driver14()
